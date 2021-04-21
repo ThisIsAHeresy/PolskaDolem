28d7a9e8763d45681a1bb940ba1d82dc739f5471
ThisIsAHeresy <umroixerta@tuta.io>
2021-04-21 10:59:02 +0200

    Zad 5

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,2 +1,13 @@
-package com.company;public class Car {
+package com.company;
+
+public class Car {
+    public final String producer;
+    public final String model;
+    Double mileage;
+    Integer value;
+
+    public Car(String model, String producer) {
+        this.model = model;
+        this.producer = producer;
+    }
 }
--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,2 +1,43 @@
-package com.company;public class Human {
+package com.company;
+
+public class Human {
+    String firstName;
+    String lastName;
+    Animal pet;
+    Phone mobilePhone;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        if (this.salary > car.value) {
+            System.out.println("You have successfully bought a car!");
+            this.car = car;
+        } else if (this.salary > car.value/12) {
+            System.out.println("You have successfully bought a car however you have to pay out monthly debt!");
+            this.car = car;
+        } else {
+            System.out.println("Start studying, find a new job or try getting a raise, idk, you cannot buy that car.");
+        }
+    }
+
+    private Car car;
+    private Double salary;
+
+    public Double getSalary() {
+        System.out.println("Salary was retrieved, current value: "+this.salary.toString());
+        return this.salary;
+    }
+    public void setSalary(Double salary) throws Exception {
+        if (salary < 0) {
+            System.out.println("Salary cannot be negative!");
+            return;
+        }
+        System.out.println("New salary has been sent to the system.");
+        System.out.println("Please remember to grab the addendum to the agreement from Hania in HR.");
+        System.out.println("ZUS and US already know about the changes.");
+        this.salary = salary;
+    }
+
 }
