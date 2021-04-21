75e2b7272d456d439bf10149de0d50597278df13
ThisIsAHeresy <umroixerta@tuta.io>
2021-04-21 08:53:11 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   Animal dog = new Animal();
+	   dog.species = "Border collie";
+	   dog.name = "Pies";
+	   dog.weight = 18.5;
+
+	   Animal cat = new Animal();
+	   cat.species = "Felis catus";
+	   cat.name = "<3";
+	   cat.weight = 2.0;
+
+	   System.out.println(dog.name);
+	   System.out.println(dog.species);
+
+	   dog.feed();
+	   dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+		dog.spacer();
+    }
+}
