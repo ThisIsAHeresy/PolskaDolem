58f0e0ba8fb0c51a7da17cd600010a342590312d
ThisIsAHeresy <umroixerta@tuta.io>
2021-05-27 16:15:30 +0200

    Ex 12

--- src/com/company/devices/Car.java
+++ src/com/company/devices/Car.java
@@ -3,20 +3,25 @@ package com.company.devices;
 import com.company.Human;
 import com.company.Salable;
 
+import java.util.LinkedList;
+
 public abstract class Car extends Device implements Salable {
     public String producer;
     public String model;
     public Integer yearOfProduction;
     Double mileage;
     public Integer value;
+    public LinkedList<Human> owners;
 
     public abstract void refuel();
 
-    public Car(String producer, String model, Integer yearOfProduction, Double value) {
+    public Car(String producer, String model, Integer yearOfProduction, Double value, Human owner) {
         super(producer, model, yearOfProduction, value);
         this.producer = producer;
         this.model = model;
         this.yearOfProduction = yearOfProduction;
+        this.owners = new LinkedList<Human>();
+        this.owners.add(owner);
     }
 
 //    @Override
@@ -45,12 +50,41 @@ public abstract class Car extends Device implements Salable {
         if (buyer.cash < price) {
             throw new Exception("Kupujący nie posiada wystarczającego hajsa");
         }
+        if (this.owners.getLast() != seller) {
+            throw new Exception("Sprzedający nie widnieje jako ostatni właściciel pojazdu.");
+        }
 
         seller.removeCar(this);
         buyer.addCar(this);
         seller.cash += price;
         buyer.cash -= price;
+        this.owners.add(buyer);
         System.out.println("Tranzakcja przebiegła pomyślnie.");
+    }
+
+    public boolean wasAnOwner(Human person) {
+        for (Human owner:
+             this.owners) {
+            if (person == owner) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean didAselltoB(Human seller, Human buyer) {
+        for (int i = 0; i < this.owners.size()-1; i++) {
+            if (this.owners.get(i) == seller) {
+                if (this.owners.get(i+1) == buyer) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int howManyTransactions() {
+        return this.owners.size()-1;
     }
 
     public int compareTo(Car car) {
